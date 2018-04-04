213c984aec0934bf45038f12bf5cd7d7022a9659
Webbcode <2020mcgillb@gmail.com>
2018-04-04 08:45:52 -0400

    Update SpriteSheetManager.js

--- SpriteSheetManager.js
+++ SpriteSheetManager.js
@@ -4,7 +4,7 @@ function SpriteSheetManager(spriteSheet, spriteSize){
   this.SSTW = 5;
   this.SSTH = 10;
   this.idMin = 0;
-  this.idMax = 28;
+  this.idMax = 29;
   
   this.renderSprite = function(id, x, y, w, h){
     if(id < this.idMax && id >= this.idMin){
